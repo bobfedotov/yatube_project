017135a9010bd7c993e5a2ad8e454401e67a4659
Leonid Fedotov <leonid.a.fedotov@yandex.ru>
2022-06-12 19:12:51 +0300

    ver.1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,22 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
+from .models import Post, Group
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Здесь можно всё, если ты {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    template = 'posts/group_list.html'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
